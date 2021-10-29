33bebfc3672d17cc8ce3d6be49ca6b6015761b4f
dgbrizan <32884156+dgbrizan@users.noreply.github.com>
2021-10-29 10:23:11 -0700

    Initial KNN.java -- untested!

---
+++ KNN.java
@@ -0,0 +1,24 @@
+
+public class KNN {
+	
+	private int k;
+	private long random_state;
+	
+	
+	public KNN(int k, long random_state) {
+		this.k = k;
+		this.random_state = random_state;
+		// TODO: YOUR ADDITIONAL CODE HERE
+	}
+	
+	
+	public void fit(double [][] features, int [] labels) {
+		// TODO: YOUR CODE HERE
+	}
+	
+	
+	public int [] predict(double [][] features) {
+		// TODO: YOUR CODE HERE
+	}
+
+}
